08a81a790134b68797858ecb9c0e44a8056edcf5
Seba <segurasebastian7887@gmail.com>
2019-01-28 12:24:41 -0300

    Working on Apollo Client implementation w/Context API

--- client/src/components/JobBoard.js
+++ client/src/components/JobBoard.js
@@ -1,29 +1,25 @@
 import React from 'react';
+import { Query } from 'react-apollo';
+import { jobsQuery } from '../libs/queries';
 import JobList from './JobList';
-import { loadJobs } from '../libs/request';
 
-class JobBoard extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      jobs: []
-    }
-  }
+const JobBoard = () => (
+  <Query query={jobsQuery}>
+    {({ data, loading }) => {
+      const { jobs } = data;
 
-  componentDidMount = async () => {
-    const jobs = await loadJobs();
-    this.setState({ jobs: jobs });
-  }
+      if (loading || !jobs) {
+        return <div>Loading...</div>
+      }
 
-  render() {
-    const { jobs } = this.state;
-    return (
-      <div>
-        <h1 className="title">Job Board</h1>
-        <JobList jobs={jobs} />
-      </div>
-    );
-  }
-}
+      return (
+        <div>
+          <h1 className='title'>Job Board</h1>
+          <JobList jobs={jobs} />
+        </div>
+      );
+    }}
+  </Query>
+)
 
 export default JobBoard;
