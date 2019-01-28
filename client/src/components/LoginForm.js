f877c8bc643fe9d6534a8e39bae4cc1effd341bc
Seba <segurasebastian7887@gmail.com>
2019-01-28 15:57:31 -0300

    Working on login

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,5 +1,9 @@
 import React from 'react';
+import { Mutation } from 'react-apollo';
+import { loginMutation } from '../libs/mutations';
 import { login } from '../libs/request';
+
+const accessTokenKey = 'accessToken';
 
 class LoginForm extends React.Component {
   constructor(props) {
@@ -24,31 +28,53 @@ class LoginForm extends React.Component {
     });
   }
 
+  completed() {
+    this.props.history.push('/');
+  }
+
   render() {
     const { email, password, error } = this.state;
     return (
-      <form>
-        <div className="field">
-          <label className="label">Email</label>
-          <div className="control">
-            <input className="input" type="text" name="email" value={email}
-              onChange={this.handleChange.bind(this)} />
-          </div>
-        </div>
-        <div className="field">
-          <label className="label">Password</label>
-          <div className="control">
-            <input className="input" type="password" name="password" value={password}
-              onChange={this.handleChange.bind(this)} />
-          </div>
-        </div>
-        <div className="field">
-          <p className="help is-danger">{error && 'Invalid credentials'}</p>
-          <div className="control">
-            <button className="button is-link" onClick={this.handleClick.bind(this)}>Login</button>
-          </div>
-        </div>
-      </form>
+      <Mutation
+        mutation={loginMutation}
+        variables={{ input: { email, password } }}
+        onCompleted={this.completed}
+      >
+        {(login, { data }) => (
+          <form
+            onSubmit={async e => {
+              e.preventDefault();
+              const { data: { me: { token } } } = await login();
+              if (token) {
+                localStorage.setItem(accessTokenKey, token);
+              } else {
+                this.setState({ error: true });
+              }
+            }}
+          >
+            <div className="field">
+              <label className="label">Email</label>
+              <div className="control">
+                <input className="input" type="text" name="email" value={email}
+                  onChange={this.handleChange.bind(this)} />
+              </div>
+            </div>
+            <div className="field">
+              <label className="label">Password</label>
+              <div className="control">
+                <input className="input" type="password" name="password" value={password}
+                  onChange={this.handleChange.bind(this)} />
+              </div>
+            </div>
+            <div className="field">
+              <p className="help is-danger">{error && 'Invalid credentials'}</p>
+              <div className="control">
+                <button className="button is-link" type="submit">Login</button>
+              </div>
+            </div>
+          </form>
+        )}
+      </Mutation>
     );
   }
 }
